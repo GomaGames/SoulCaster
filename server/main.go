644047872fc29ab040a39826c91f7a72e206731c
Jon Borgonia <jon@gomagames.com>
2018-01-27 13:02:43 -0800

    improve host port flags

--- server/main.go
+++ server/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+var (
+	host string
+	port string
+)
+
 const (
-	defaultPort = "8000"
+	defaultHost  = ""
+	hostFlagDesc = "server -h xx.yy.zz.xx"
+	defaultPort  = "8000"
+	portFlagDesc = "server -p 8000"
 )
 
 func main() {
+	flag.StringVar(&host, "host", defaultHost, hostFlagDesc)
+	flag.StringVar(&host, "h", defaultHost, hostFlagDesc)
+	flag.StringVar(&port, "port", defaultPort, portFlagDesc)
+	flag.StringVar(&port, "p", defaultPort, portFlagDesc)
+	flag.Parse()
 	http.Handle("/", http.FileServer(http.Dir("../public")))
 
-	fmt.Printf("Server listening on %s\n", defaultPort)
-	log.Fatal(http.ListenAndServe(":"+defaultPort, nil))
+	addr := host + ":" + port
+	fmt.Printf("Server listening on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
