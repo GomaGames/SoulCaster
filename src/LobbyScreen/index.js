2869f489ee1ece4d45603ebe1c43aad2d57a14a8
Te Vallee <astronomemusic@gmail.com>
2018-01-26 23:25:17 -1000

    lobby screen component renders Play button and routes to Game screen. closes #5

--- src/LobbyScreen/index.js
+++ src/LobbyScreen/index.js
@@ -3,9 +3,21 @@ import * as React from 'react';
 type Props = {};
 
 class LobbyScreen extends React.Component<Props> {
+
+  play = (event: SyntheticEvent<HTMLButtonElement>) => {
+    (event.currentTarget: HTMLButtonElement);
+
+    this.props.history.push('/game')
+  }
+
   render() {
 
-    return <h1>Lobby</h1>;
+    return (
+      <div className="lobby-screen">
+        <h1>Waiting for Players</h1>
+        <button className="button play-button" type="button" onClick={ this.play }>Play</button>
+      </div>
+    );
   }
 }
 
