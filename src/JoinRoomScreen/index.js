51e5a83957f39b1cfce586d8522bc0202148d74f
Te Vallee <astronomemusic@gmail.com>
2018-01-26 23:22:03 -1000

    join screen component renders Join Room button and routes to Lobby screen. closes #4

--- src/JoinRoomScreen/index.js
+++ src/JoinRoomScreen/index.js
@@ -3,9 +3,22 @@ import * as React from 'react';
 type Props = {};
 
 class JoinRoomScreen extends React.Component<Props> {
+
+	join = (event: SyntheticEvent<HTMLButtonElement>) => {
+    (event.currentTarget: HTMLButtonElement);
+
+    this.props.history.push('/lobby')
+  }
+
   render() {
 
-    return <h1>Join</h1>;
+    return (
+    	<div className="join-room-screen">
+    		<h1>Join Room</h1>
+    		<input type="text" placeholder="Room Code" />
+    		<button className="button join-button" type="button" onClick={ this.join }>Join</button>
+    	</div>
+    );
   }
 }
 
