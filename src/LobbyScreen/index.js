7656a9ff4b05888c5c1bafe64c6ba633ca08f8df
Te Vallee <astronomemusic@gmail.com>
2018-01-27 12:10:23 -1000

    LobbyScreen component edits + styles

--- src/LobbyScreen/index.js
+++ src/LobbyScreen/index.js
@@ -1,21 +1,39 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
 
-type Props = {};
+type Props = {
+  player2: Object
+};
 
 class LobbyScreen extends React.Component<Props> {
 
-  play = (event: SyntheticEvent<HTMLButtonElement>) => {
+  startGame = (event: SyntheticEvent<HTMLButtonElement>) => {
     (event.currentTarget: HTMLButtonElement);
 
     this.props.history.push('/game')
   }
 
   render() {
+    let content = null;
+
+    if(this.props.player) {
+      content = <button className="button start-game-button" type="button" onClick={ this.startGame }>Start Game</button>
+    } else {
+      content = <div>
+        <p>Tell your opponent this code:</p>
+        <div className="code">
+          <p>XLRT</p>
+        </div>
+      </div>
+    }
 
     return (
-      <div className="lobby-screen">
-        <h1>Waiting for Players</h1>
-        <button className="button play-button" type="button" onClick={ this.play }>Play</button>
+      <div className="lobby-screen screen">
+        <h1>Ready?</h1>
+        <p className="subtitle">{ this.props.player2 ? 'It\'s time for battle!' : 'Waiting for Player 2...' }</p>
+        { content }
+        <Link className='exit-button' to='/'>&#8249; Exit</Link>
       </div>
     );
   }
