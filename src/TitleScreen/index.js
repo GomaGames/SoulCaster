27be6e330cceac9c8fd93de8dd13b167ab91201f
Te Vallee <astronomemusic@gmail.com>
2018-01-26 23:16:43 -1000

    title screen component renders Create Room and Join Room buttons. closes #3

--- src/TitleScreen/index.js
+++ src/TitleScreen/index.js
@@ -3,9 +3,27 @@ import * as React from 'react';
 type Props = {};
 
 class TitleScreen extends React.Component<Props> {
+  createRoom = (event: SyntheticEvent<HTMLButtonElement>) => {
+    (event.currentTarget: HTMLButtonElement);
+
+    this.props.history.push('/lobby')
+  }
+
+  joinRoom = (event: SyntheticEvent<HTMLButtonElement>) => {
+    (event.currentTarget: HTMLButtonElement);
+
+    this.props.history.push('/join')
+  }
+
   render() {
 
-    return <h1>Title Screen</h1>;
+    return (
+      <div className="title-screen">
+        <h1>Codename Transmission</h1>
+        <button className="button create-room-button" type="button" onClick={ this.createRoom }>Create Room</button>
+        <button className="button join-room-button" type="button" onClick={ this.joinRoom }>Join Room</button>
+      </div>
+    );
   }
 }
 
