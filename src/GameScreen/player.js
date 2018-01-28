221d88ab12496fc56196eca4fbae19b0529ad464
Jon Borgonia <jon@gomagames.com>
2018-01-28 12:50:37 -0800

    GameScreen other player attacking has weapon

--- src/GameScreen/player.js
+++ src/GameScreen/player.js
@@ -14,36 +14,20 @@ const Player = ({
   let weapon = '';
   if( number === playerNumber ){ // self
     characterSrc = characterSrc[animationFrame][`color${number}`];
-    // if( number === 1){ // on left
-      if( animationFrame === AnimationFrame.ATTACK ){
-        componentClassNames.push(`player-${number}-attack`);
-        weapon = <img className="attack-weapon" src={ weaponSrc } alt="weapon"/>;
-      } else if( animationFrame === AnimationFrame.HIT ){
-        componentClassNames.push(`player-${number}-hit`) 
-      }
-    // } else { // on right
-    //   if( animationFrame === AnimationFrame.ATTACK ){
-    //     componentClassNames.push(`player-${number}-attack`) 
-    //   } else if( animationFrame === AnimationFrame.HIT ){
-    //     componentClassNames.push(`player-${number}-hit`) 
-    //   }
-    // }
+    if( animationFrame === AnimationFrame.ATTACK ){
+      componentClassNames.push(`player-${number}-attack`);
+      weapon = <img className="attack-weapon" src={ weaponSrc } alt="weapon"/>;
+    } else if( animationFrame === AnimationFrame.HIT ){
+      componentClassNames.push(`player-${number}-hit`) 
+    }
   } else { // opponent
     characterSrc = characterSrc[opponentAnimationFrame][`color${number}`];
-    // if( number === 1){ // on left
-      if( opponentAnimationFrame === AnimationFrame.ATTACK ){
-        componentClassNames.push(`player-${number}-attack`) 
-      } else if( opponentAnimationFrame === AnimationFrame.HIT ){
-        componentClassNames.push(`player-${number}-hit`) 
-      }
-    // } else { // on right
-    //   if( animationFrame === AnimationFrame.ATTACK ){
-    //     componentClassNames.push(`player-${number}-attack`) 
-    //   } else if( animationFrame === AnimationFrame.HIT ){
-    //     componentClassNames.push(`player-${number}-hit`) 
-    //   }
-    // }
-
+    if( opponentAnimationFrame === AnimationFrame.ATTACK ){
+      componentClassNames.push(`player-${number}-attack`) 
+      weapon = <img className="attack-weapon" src={ weaponSrc } alt="weapon"/>;
+    } else if( opponentAnimationFrame === AnimationFrame.HIT ){
+      componentClassNames.push(`player-${number}-hit`) 
+    }
   }
 
 
